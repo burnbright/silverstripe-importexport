553c3684762fbde9bbfa234df933bead1f98dcc6
Jeremy Shipman <jeremy@burnbright.net>
2015-03-30 09:54:58 +1300

    Minor doc update to BulkLoaderSource

--- code/BulkLoaderSource.php
+++ code/BulkLoaderSource.php
@@ -1,9 +1,10 @@
 <?php
 
-
 /**
  * An abstract source to bulk load records from.
  * Provides an iterator for retrieving records from.
+ * 
+ * Useful for holiding source configuration state.
  */
 abstract class BulkLoaderSource{
 
